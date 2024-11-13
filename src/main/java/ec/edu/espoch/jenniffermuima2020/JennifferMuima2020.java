31fa86754160248b040217a5b288e1786eb8ea84
jenniffermuima. <jenniffermuima2001@gmail.com>
2024-11-13 14:52:03 -0500

    Jenniffer

---
+++ src/main/java/ec/edu/espoch/jenniffermuima2020/JennifferMuima2020.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.jenniffermuima2020;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class JennifferMuima2020 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
